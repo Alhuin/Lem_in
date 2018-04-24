69d36efa36169cb4720c09bacb24f1ea35daf3e1
Julien Janin-reynaud <jjanin-r@z3r1p2.le-101.fr>
2018-04-24 20:44:35 +0200

    free_tree added

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: jjanin-r <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/04/24 20:29:21 by jjanin-r     #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/24 20:34:50 by jjanin-r    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/24 20:44:14 by jjanin-r    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,15 +15,30 @@
 
 void		free_node(t_forest **node)
 {
-	ft_strdel(&(*node)->room->name);
+	int	i;
+	int links;
 
+	links = (*node)->room->nb_links;
+	i = -1;
+	ft_strdel(&(*node)->room->name);
+	free((*node)->room);
+	while (++i < links)
+		free((*node)->subtree[i]);
+	free((*node)->parent);
+	free((*node)->subtree);
+	free(*node);
 }
 
 void		free_tree(t_forest **tree)
 {
+	int i;
+
+	i = -1;
 	if ((*tree)->subtree)
 	{
-		free_tree((*tree)->subtree);
+		while (++i < (*tree)->room->nb_links)
+			free_tree(&(*tree)->subtree[i]);
+	}
 	if (*tree)
 		free_node(tree);
 }
